325926a4cf35ce09df7d5266b4648a0fb892174f
Gurleen Kaur <73643543+gurleenMaini@users.noreply.github.com>
2021-12-07 14:51:10 +0530

    Update Project98.py

--- Project98.py
+++ Project98.py
@@ -5,9 +5,9 @@ def swapFileData():
     data_a = a.read()
   with open(file2, 'r') as b: 
     data_b = b.read()
-  with open(file1, 'w+') as a: 
+  with open(file1, 'w') as a: 
     a.write(data_b)
-  with open(file2, 'W+') as b: 
+  with open(file2, 'w') as b: 
     b.write(data_a)
   print("Your file has been swapped!")
 
